86d66027dba6a24d5628e0c9cfa58bf129376171
Sanders Lauture <golf1052@live.com>
2015-06-18 00:50:38 -0400

    Split View Frame Thing
    In other words. I have no idea what I'm doing.

---
+++ TwixelAppUniversal/AppShell.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace TwixelAppUniversal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class AppShell : Page
+    {
+        public static AppShell Current = null;
+        public Frame AppFrame { get { return frame; } }
+
+        public AppShell()
+        {
+            this.InitializeComponent();
+
+            this.Loaded += AppShell_Loaded;
+        }
+
+        private void AppShell_Loaded(object sender, RoutedEventArgs e)
+        {
+            Current = this;
+        }
+    }
+}
