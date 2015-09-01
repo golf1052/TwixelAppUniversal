b0647db0ecf92f6dcb289e85f17d18af7999737d
Sanders Lauture <golf1052@live.com>
2015-08-31 22:44:24 -0400

    Chat emotes aren't broken as hell anymore
    Basically WrapPanel. Started setting up ChatWindow but this is a big
    step.

--- TwixelAppUniversal/Binding Classes/ChatListViewBinding.cs
+++ TwixelAppUniversal/Binding Classes/ChatListViewBinding.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using TwixelChat;
 using TwixelEmotes;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace TwixelAppUniversal
@@ -16,9 +19,11 @@ namespace TwixelAppUniversal
         public string Username { get; set; }
         public string Color { get; set; }
         public string Message { get; set; }
+        public ObservableCollection<UIElement> ChatThings { get; set; }
 
         public ChatListViewBinding(ChatMessage message, string channel)
         {
+            ChatThings = new ObservableCollection<UIElement>();
             if (message.User != null)
             {
                 if (message.User.UserType == UserState.UserTypes.Admin)
@@ -105,7 +110,59 @@ namespace TwixelAppUniversal
                 Username = message.Username;
                 Color = "#000000";
             }
-            Message = message.Message;
+
+            if (message.Emotes == null || message.Emotes.Count == 0)
+            {
+                TextBlock textBlockMessage = new TextBlock();
+                textBlockMessage.Text = message.Message;
+                textBlockMessage.TextWrapping = TextWrapping.WrapWholeWords;
+                ChatThings.Add(textBlockMessage);
+            }
+            else
+            {
+                SortedDictionary<long, ChatEmote> sortedEmotes = new SortedDictionary<long, ChatEmote>();
+                foreach (ChatEmote emote in message.Emotes)
+                {
+                    foreach (Tuple<long, long> position in emote.Positions)
+                    {
+                        sortedEmotes.Add(position.Item1, emote);
+                    }
+                }
+                int currentPosition = 0;
+                foreach (KeyValuePair<long, ChatEmote> emote in sortedEmotes)
+                {
+                    TextBlock textBlockMessage = new TextBlock();
+                    textBlockMessage.Text = message.Message.Substring(currentPosition, (int)emote.Key - currentPosition);
+                    textBlockMessage.TextWrapping = TextWrapping.WrapWholeWords;
+                    ChatThings.Add(textBlockMessage);
+                    Image image = new Image();
+                    BitmapImage bitmapImage = new BitmapImage(EmoteManager.EmotesById[emote.Value.Id].Small);
+                    image.Source = bitmapImage;
+                    image.Stretch = Windows.UI.Xaml.Media.Stretch.None;
+                    ChatThings.Add(image);
+                    var position = FindTuple(emote.Value.Positions, emote.Key);
+                    currentPosition = (int)position.Item2 + 1;
+                }
+                if (currentPosition < message.Message.Length)
+                {
+                    TextBlock textBlockMessage = new TextBlock();
+                    textBlockMessage.Text = message.Message.Substring(currentPosition);
+                    textBlockMessage.TextWrapping = TextWrapping.WrapWholeWords;
+                    ChatThings.Add(textBlockMessage);
+                }
+            }
+        }
+
+        private Tuple<long, long> FindTuple(List<Tuple<long, long>> tuples, long key)
+        {
+            foreach (var tuple in tuples)
+            {
+                if (tuple.Item1 == key)
+                {
+                    return tuple;
+                }
+            }
+            throw new Exception("Couldn't find tuple");
         }
     }
 }
---
+++ TwixelAppUniversal/ChatWindow.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwixelChat.Universal;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace TwixelAppUniversal
+{
+    public class ChatWindow
+    {
+        ChatClient chatClient;
+        ObservableCollection<ChatListViewBinding> chatMessages;
+        private CoreDispatcher Dispatcher { get; set; }
+        ListView chatListView;
+        ScrollViewer scrollViewer;
+        string channelName;
+
+        bool loadedChatView;
+
+        public ChatWindow(CoreDispatcher dispatcher,
+            string channelName,
+            ListView chatView)
+        {
+            Dispatcher = dispatcher;
+            this.channelName = channelName;
+            chatListView = chatView;
+
+            loadedChatView = false;
+            
+            chatMessages = new ObservableCollection<ChatListViewBinding>();
+
+            chatListView.Loaded += ChatListView_Loaded;
+
+            chatClient = new ChatClient();
+            chatClient.MessageRecieved += ChatClient_MessageRecieved;
+        }
+
+        private void ChatListView_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (!loadedChatView)
+            {
+                loadedChatView = true;
+                LoadChatView();
+            }
+        }
+
+        private void LoadChatView()
+        {
+            chatListView.ItemsSource = chatMessages;
+        }
+
+        private void ChatClient_MessageRecieved(object sender, TwixelChat.Events.MessageRecievedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
