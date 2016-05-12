5716c42ba2d111f134f069b3185eb0a8f4eb7f32
Sanders Lauture <golf1052@msn.com>
2016-05-12 13:01:29 -0400

    Create Secrets.example.cs

---
+++ Secrets.example.cs
@@ -0,0 +1,10 @@
+namespace TwixelAppUniversal
+{
+    public static class Secrets
+    {
+        public const string ClientId = "";
+        public const string ClientSecret = "";
+        public const string RedirectUrl = "";
+        public const string AccessToken = "";
+    }
+}
